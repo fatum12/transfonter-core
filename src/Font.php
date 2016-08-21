1129c846936b3abfeb3999f83a74864c3e242dd8
Aleksey Ilyaskin <doc-doom@ya.ru>
2016-08-21 20:17:28 +0300

    Add more subsets

--- src/Font.php
+++ src/Font.php
@@ -22,10 +22,14 @@ class Font
 	const SUBSET_GREEK = 'greek';
 	const SUBSET_GREEK_EXT = 'greek-ext';
 	const SUBSET_GUJARATI = 'gujarati';
+    const SUBSET_GURMUKHI = 'gurmukhi';
 	const SUBSET_HEBREW = 'hebrew';
+    const SUBSET_KANNADA = 'kannada';
 	const SUBSET_KHMER = 'khmer';
 	const SUBSET_LATIN = 'latin';
 	const SUBSET_LATIN_EXT = 'latin-ext';
+    const SUBSET_MALAYALAM = 'malayalam';
+    const SUBSET_ORIYA = 'oriya';
 	const SUBSET_TAMIL = 'tamil';
 	const SUBSET_TELUGU = 'telugu';
 	const SUBSET_THAI = 'thai';
@@ -62,13 +66,18 @@ class Font
 		self::SUBSET_GREEK => ['U+0370-03FF'],
 		self::SUBSET_GREEK_EXT => ['U+1F00-1FFF'],
 		self::SUBSET_GUJARATI => ['U+0964-0965', 'U+0A80-0AFF', 'U+200B-200D', 'U+20B9', 'U+25CC', 'U+A830-A839'],
+        self::SUBSET_GURMUKHI => ['U+0964-0965', 'U+0A01-0A75', 'U+200B-200D', 'U+20B9', 'U+25CC', 'U+262C',
+            'U+A830-A839'],
 		self::SUBSET_HEBREW => ['U+0590-05FF', 'U+20AA', 'U+25CC', 'U+FB1D-FB4F'],
+        self::SUBSET_KANNADA => ['U+0964-0965', 'U+0C82-0CF2', 'U+200B-200D', 'U+20B9', 'U+25CC'],
 		self::SUBSET_KHMER => ['U+1780-17FF', 'U+200B-200C', 'U+25CC'],
 		self::SUBSET_LATIN => ['U+0000-00FF', 'U+0131', 'U+0152-0153', 'U+02C6', 'U+02DA', 'U+02DC', 'U+2000-206F',
 			'U+2074', 'U+20AC', 'U+2212', 'U+2215', 'U+E0FF', 'U+EFFD', 'U+F000'],
 		self::SUBSET_LATIN_EXT => ['U+0100-024F', 'U+1E00-1EFF', 'U+20A0-20AB', 'U+20AD-20CF', 'U+2C60-2C7F',
 			'U+A720-A7FF'],
-		self::SUBSET_TAMIL => ['U+0964-0965', 'U+0B82-0BFA', 'U+200B-200D', 'U+20B9', 'U+25CC'],
+        self::SUBSET_MALAYALAM => ['U+0307', 'U+0323', 'U+0964-0965', 'U+0D02-0D7F', 'U+200B-200D', 'U+20B9', 'U+25CC'],
+		self::SUBSET_ORIYA => [''],
+        self::SUBSET_TAMIL => ['U+0964-0965', 'U+0B82-0BFA', 'U+200B-200D', 'U+20B9', 'U+25CC'],
 		self::SUBSET_TELUGU => ['U+0951-0952', 'U+0964-0965', 'U+0C00-0C7F', 'U+1CDA', 'U+200C-200D', 'U+25CC'],
 		self::SUBSET_THAI => ['U+0E01-0E5B', 'U+200B-200D', 'U+25CC'],
 		self::SUBSET_VIETNAMESE => ['U+0102-0103', 'U+1EA0-1EF1', 'U+20AB'],
@@ -103,8 +112,12 @@ class Font
 			self::SUBSET_GREEK => 'Greek',
 			self::SUBSET_GREEK_EXT => 'Greek Extended',
 			self::SUBSET_GUJARATI => 'Gujarati',
+            self::SUBSET_GURMUKHI => 'Gurmukhi',
 			self::SUBSET_HEBREW => 'Hebrew',
+            self::SUBSET_KANNADA => 'Kannada',
 			self::SUBSET_KHMER => 'Khmer',
+            self::SUBSET_MALAYALAM => 'Malayalam',
+            self::SUBSET_ORIYA => 'Oriya',
 			self::SUBSET_TAMIL => 'Tamil',
 			self::SUBSET_TELUGU => 'Telugu',
 			self::SUBSET_THAI => 'Thai',
