b4e0c16f828e98cac50bbe72e6725452ccb09e00
Aleksey Ilyaskin <doc-doom@ya.ru>
2017-10-30 22:08:21 +0300

    ttfautohint configuration

--- src/Tools/Ttfautohint.php
+++ src/Tools/Ttfautohint.php
@@ -8,7 +8,7 @@ class Ttfautohint
     public static function autohint($source, $target)
     {
         $command = sprintf(
-            'ttfautohint --strong-stem-width="" --windows-compatibility --composites -i "%s" "%s"',
+            'ttfautohint --windows-compatibility --composites -i "%s" "%s"',
             $source,
             $target
         );
