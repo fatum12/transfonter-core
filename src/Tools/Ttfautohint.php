736c7f116f90478ee8db9c855c8c0fa28b0b9c26
Aleksey Ilyaskin <doc-doom@ya.ru>
2018-06-02 16:55:22 +0300

    ttfautohint options

--- src/Tools/Ttfautohint.php
+++ src/Tools/Ttfautohint.php
@@ -8,7 +8,7 @@ class Ttfautohint
     public static function autohint($source, $target)
     {
         $command = sprintf(
-            'ttfautohint --windows-compatibility --composites -i "%s" "%s"',
+            'ttfautohint --windows-compatibility --composites -i -X "-" "%s" "%s"',
             $source,
             $target
         );
